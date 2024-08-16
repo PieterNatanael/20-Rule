16e7cf9e537dfd5a62ba2c0ababa945a8a4a095e
PieterNatanael <pieternatanael@icloud.com>
2024-08-16 15:00:55 +0800

    Initial Commit

---
+++ 20 Rule/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  20 Rule
+//
+//  Created by Pieter Yoshua Natanael on 16/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 20 Rule/_0_RuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0_RuleApp.swift
+//  20 Rule
+//
+//  Created by Pieter Yoshua Natanael on 16/08/24.
+//
+
+import SwiftUI
+
+@main
+struct _0_RuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
